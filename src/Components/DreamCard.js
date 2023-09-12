424066b35b1a8fe9ae518f440941258670214f69
Quartermaster-90 <christopher.belsey@icloud.com>
2023-09-12 16:47:13 +0100

    dream journal page complete

--- src/Components/DreamCard.js
+++ src/Components/DreamCard.js
@@ -12,20 +12,40 @@ const DreamCard = ({dream, removeDream}) => {
     return (
         <>
             <div className="dreamjournal-container">
-            <div>
-            <h1 className='title-dreamjournal'>{dream.title}</h1>
-            <p className='body-dreamjournal'>{dream.body}</p>
-            <div className='tags-dreamjournal'>
-            <ul class="nav">
-                <li><b>Lucid Dream:</b> <span>{dream.isLucidDream ? '✅' : '❌'}</span></li>
-                <li><b>Nightmare:</b> <span>{dream.isNightmare ? '✅' : '❌'}</span></li>
-                <li><b>Recurring:</b> <span>{dream.isRecurring ? '✅' : '❌'}</span></li>
-            </ul>
-            </div>
-            <p><b>Date:</b> {dream.date}</p>
-            <button onClick={handleDelete}> 🗑 </button>
-            <hr></hr>
-            </div>
+            <style>
+            @import url('https://fonts.googleapis.com/css2?family=Caprasimo&family=Pacifico&display=swap');
+            </style>
+                <div class="blog_post">
+                    <div class="img_pod">
+                        <img src="/user_picture.jpg" alt="User Picture"/>
+                    </div>
+                    <div class="container_copy">
+                    <h3><b>Date:</b> {dream.date}</h3>
+                    <h1 style={{ fontFamily: 'Caprasimo, cursive' }}>{dream.title}</h1>
+                    <p style={{ fontFamily: 'Pacifico, cursive' }}>{dream.body}.</p>
+                    <div className='tags-dreamjournal'>
+                            <ul class="nav">
+                                <li><b>Lucid Dream:</b> <span>{dream.isLucidDream ? '✅' : '❌'}</span></li>
+                                <li><b>Nightmare:</b> <span>{dream.isNightmare ? '✅' : '❌'}</span></li>
+                                <li><b>Recurring:</b> <span>{dream.isRecurring ? '✅' : '❌'}</span></li>
+                            </ul>
+                        </div>
+                    <a class="btn_primary" onClick={handleDelete}> 🗑 </a>
+                    </div>
+                </div>
+                {/* <div className="dreamjournal-entry">
+                    <h1 className='title-dreamjournal'>{dream.title}</h1>
+                    <p className='body-dreamjournal'>{dream.body}</p>
+                        <div className='tags-dreamjournal'>
+                            <ul class="nav">
+                                <li><b>Lucid Dream:</b> <span>{dream.isLucidDream ? '✅' : '❌'}</span></li>
+                                <li><b>Nightmare:</b> <span>{dream.isNightmare ? '✅' : '❌'}</span></li>
+                                <li><b>Recurring:</b> <span>{dream.isRecurring ? '✅' : '❌'}</span></li>
+                            </ul>
+                        </div>
+                    <p className="date"><b>Date:</b> {dream.date}</p>
+                    <button className="delete-button" onClick={handleDelete}> 🗑 </button>
+                </div> */}
             </div>
         </>
     )
