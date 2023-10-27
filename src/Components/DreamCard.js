d540a45ab968ac6825985ddd436429e152c947a2
Quartermaster-90 <christopher.belsey@icloud.com>
2023-10-27 13:00:06 +0100

    last project version

--- src/Components/DreamCard.js
+++ src/Components/DreamCard.js
@@ -34,19 +34,6 @@ const DreamCard = ({dream, removeDream}) => {
                     <a class="btn_primary" onClick={handleDelete}> 🗑 </a>
                     </div>
                 </div>
-                {/* <div className="dreamjournal-entry">
-                    <h1 className='title-dreamjournal'>{dream.title}</h1>
-                    <p className='body-dreamjournal'>{dream.body}</p>
-                        <div className='tags-dreamjournal'>
-                            <ul class="nav">
-                                <li><b>Lucid Dream:</b> <span>{dream.isLucidDream ? '✅' : '❌'}</span></li>
-                                <li><b>Nightmare:</b> <span>{dream.isNightmare ? '✅' : '❌'}</span></li>
-                                <li><b>Recurring:</b> <span>{dream.isRecurring ? '✅' : '❌'}</span></li>
-                            </ul>
-                        </div>
-                    <p className="date"><b>Date:</b> {dream.date}</p>
-                    <button className="delete-button" onClick={handleDelete}> 🗑 </button>
-                </div> */}
             </div>
         </>
     )
